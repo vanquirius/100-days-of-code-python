8baeabd40521929a3c08660241f85cb2a971d2ce
Marcelo Ambrosio de Góes <marcelogoes@gmail.com>
2022-01-30 22:50:52 -0300

    first version of full program

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -6,10 +6,17 @@
 # 100 Days of Code: The Complete Python Pro Bootcamp for 2022
 # Day 15 - Coffee Machine
 
-# Data provided by the exercise
+# print debug (must be 1 to print)
+verbose = 0
 
-# print comments
-verbose = 1
+class colors:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    ENDC = '\033[m'
+
+# Data provided by the exercise
 
 MENU = {
     "expresso": {
@@ -50,21 +57,23 @@ money = 2.5
 # Requirement 1: Prompt user by asking "What would you like?"
 def capture_user_input():
     # Prompt user for input
-    raw_user_input = input("What would you like? (expresso, latte, cappuccino)")
+    raw_user_input = input(colors.BLUE + "What would you like? (expresso, latte, cappuccino)" + colors.ENDC)
     # Validate user input
     if raw_user_input not in ("expresso", "latte", "cappuccino", "off", "report"):
-        print("Invalid option. Please choose again between expresso, latte and cappuccino.")
+        print(colors.RED + "Invalid option. Please choose again between expresso, latte and cappuccino." + colors.ENDC)
         raw_user_input = capture_user_input()
     elif raw_user_input in ("off", "report"):
-        print("You chose the hidden option: " + raw_user_input + "!")
+        if verbose == 1:
+          print("You chose the hidden option: " + raw_user_input + "!")
     else:
-        print("You chose " + raw_user_input + "!")
+        if verbose == 1:
+          print("You chose " + raw_user_input + "!")
     return raw_user_input
 
 
 # Requirement 2: Turn off Coffee Machine
 def turn_off_coffee_machine():
-    print("Turning off Coffee Machine...")
+    print(colors.RED + "Turning off Coffee Machine...")
     # Stops running program
     exit()
 
@@ -105,15 +114,111 @@ def check_resources_sufficient(input_menu, input_resources, input_user_input):
     # Check resources
     if verbose == 1:
         print("Verifying if resources are enough...")
+    enough_resources = 1
     if input_resources.get("water") < water:
         print("Sorry, there is not enough water.")
+        enough_resources = 0
     if input_resources.get("milk") < milk:
         print("Sorry, there is not enough milk.")
+        enough_resources = 0
     if input_resources.get("coffee") < coffee:
         print("Sorry, there is not enough coffee.")
+        enough_resources = 0
+    if enough_resources == 1 and verbose == 1:
+        print("Resources are enough.")
+    return enough_resources
+
+# Requirement 5: Process Coins
+def process_coins():
+  def collect_quarters():
+      quarters = 0
+      try:
+          quarters = int(input("How many quarters?"))
+      except:
+          collect_quarters()
+      if quarters is None:
+        quarters = 0
+      if quarters < 0:
+        collect_quarters()
+      return quarters
+  quarters = collect_quarters()
+
+  def collect_dimes():
+      dimes = 0
+      try:
+          dimes = int(input("How many dimes?"))
+      except:
+          collect_dimes()
+      if dimes is None:
+        dimes = 0
+      if dimes < 0:
+        collect_dimes()
+      return dimes
+  dimes = collect_dimes()
+
+  def collect_nickels():
+      nickels = 0
+      try:
+          nickels = int(input("How many nickels?"))
+      except:
+          collect_nickels()
+      if nickels is None:
+        nickels = 0
+      if nickels < 0:
+        collect_nickels()
+      return nickels
+  nickels = collect_nickels()
+
+  def collect_pennies():
+      pennies = 0
+      try:
+          pennies = int(input("How many pennies?"))
+      except:
+          collect_pennies()
+      if pennies is None:
+        pennies = 0
+      if pennies < 0:
+        collect_pennies()
+      return pennies
+  pennies = collect_pennies()
+
+  deposit = round(quarters*0.25 + dimes*0.10 + nickels*0.05 + pennies*0.01,2)
+
+  if verbose == 1:
+    print("Deposited value is $" + str(deposit))
+
+  return deposit
+
+# Requirement 6: Check transaction successful
+def check_transaction(input_user_input, input_menu, input_deposit):
+  # Get cost from menu
+  cost = input_menu.get(input_user_input).get("cost")
+  # Validate cost
+  if cost < 0 or cost is None:
+      raise ValueError("Invalid cost in menu, please verify.")
+  if verbose == 1:
+    print("Cost is : $" + str(cost))
+  # Process transaction
+  transaction_success = 1
+  if cost == input_deposit:
+    if verbose == 1:
+      print("Cost and deposit are the same, no change given.")
+      change = 0
+  elif cost > input_deposit:
+    transaction_success = 0
+    print("Sorry, that is not enough money. Money refunded.")
+  else:
+    change = round(input_deposit - cost, 2)
+    print("Change of $" + str(change) + " given out.")
+  
+  return cost, transaction_success
+
+# Requirement 7: Make Coffee
+def make_drink(input_user_input):
+  print("Here is your " + input_user_input + ". Enjoy!")
 
 # Runtime
-print("Coffee Machine")
+print(colors.GREEN + "Coffee Machine" + colors.ENDC)
 
 if verbose == 1:
     print("Running Coffee Machine program with verbose output")
@@ -121,15 +226,17 @@ if verbose == 1:
     print(resources)
     print("Money: $" + str(money))
 
-user_input = capture_user_input()
-if user_input == "off":
-    turn_off_coffee_machine()
-if user_input == "report":
-    generate_report(resources, money)
-if user_input in ("expresso", "latte", "cappuccino"):
-    check_resources_sufficient(MENU, resources, user_input)
-
-# TODO 4: Check resources sufficient?
-# TODO 5: Process coins
-# TODO 6: Check transaction successful?
-# TODO 7: Make Coffee
+while(True):
+  user_input = capture_user_input()
+  if user_input == "off":
+      turn_off_coffee_machine()
+  if user_input == "report":
+      generate_report(resources, money)
+  if user_input in ("expresso", "latte", "cappuccino"):
+      enough_resources = check_resources_sufficient(MENU, resources, user_input)
+      if enough_resources == 1:
+        deposit = process_coins()
+        money_add, transaction_success = check_transaction(user_input, MENU, deposit)
+        money = money + money_add
+        if transaction_success == 1:
+          make_drink(user_input)
