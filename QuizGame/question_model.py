97e1db9dca2db8dd4d29e8913b3de5d0b0f81607
Marcelo Ambrosio de Góes <marcelogoes@gmail.com>
2022-02-06 17:38:53 -0300

    Add files via upload

---
+++ QuizGame/question_model.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# Marcelo Ambrosio de Goes
+# marcelogoes@gmail.com
+# 2022-02-05
+
+# 100 Days of Code: The Complete Python Pro Bootcamp for 2022
+# Day 17 - Quiz Game
+
+class Question:
+  def __init__(self, text, answer):
+    self.text = text
+    self.answer = answer
