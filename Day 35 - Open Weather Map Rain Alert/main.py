1032e6e990ef9a2ea7ea3f7de69089f6e2505c0d
Marcelo Góes <marcelogoes@gmail.com>
2022-03-20 23:15:07 -0300

    new version

--- Day 35 - Open Weather Map Rain Alert/main.py
+++ Day 35 - Open Weather Map Rain Alert/main.py
@@ -7,21 +7,57 @@
 # Day 35 - Open Weather Map Rain Alert
 
 import requests
-from datetime import datetime
-import time
+from twilio.rest import Client
 
 # Coordinates for São Paulo, SP, Brazil
 MY_LAT = -23.550520
 MY_LONG = -46.633308
 
-def get_iss_position():
-    # Get ISS latitude and longitude
-    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# API Keys for Open Weather Maps and Twilio
+owp_api_key = "######"
+account_sid = "######"
+auth_token = "######"
+twilio_phone_number = "######"
+to_phone_number = "######"
+
+def send_sms(input_account_sid, input_auth_token, input_twilio_phone_number, input_to_phone_number):
+    client = Client(input_account_sid, input_auth_token)
+
+    message = client.messages \
+                    .create(
+                         body="Bring an umbrella",
+                         from_=input_twilio_phone_number,
+                         to=input_to_phone_number
+                     )
+
+    print(message.sid)
+    print(message.status)
+
+def get_owp_onecallapi():
+    # Call One Call API from Open Weather Maps
+    parameters = {
+        "lat": MY_LAT,
+        "lon": MY_LONG,
+        "appid": owp_api_key,
+        "units": "metric",
+        "exclude": "current,minutely,daily"
+    }
+
+    response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+    # print("Reponse code: " + str(response.status_code))
     response.raise_for_status()
     data = response.json()
 
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
-    print("ISS Latitude: " + str(iss_latitude))
-    print("ISS Longitude: " + str(iss_longitude))
-    return iss_latitude, iss_longitude
+    # Check if we have a weather code of less than 700 in the next 12 hours
+    bad_weather = False
+    for i in range(0, 11):
+        code = data["hourly"][i]["weather"][0]["id"]
+        if code < 700:
+            bad_weather = True
+
+    # Print "bring an umbrella" if there is bad weather ahead
+    if bad_weather:
+        print("Bring an umbrella")
+        send_sms(account_sid, auth_token, twilio_phone_number, to_phone_number)
+
+get_owp_onecallapi()
