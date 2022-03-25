65cca1ad8086d0483df9d20d3813439b654479ea
Marcelo Góes <marcelogoes@gmail.com>
2022-03-25 20:46:44 -0300

    new version

--- Day 39 - Flight Deal Finder/flight_search.py
+++ Day 39 - Flight Deal Finder/flight_search.py
@@ -6,6 +6,15 @@
 # 100 Days of Code: The Complete Python Pro Bootcamp for 2022
 # Day 39 - Flight Deal Finder
 
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
