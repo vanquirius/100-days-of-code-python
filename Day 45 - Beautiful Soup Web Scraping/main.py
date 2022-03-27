3c83d6c7f1358923075ae16820acc0fc8cdf6a9e
Marcelo Góes <marcelogoes@gmail.com>
2022-03-27 15:25:56 -0300

    new version

--- Day 45 - Beautiful Soup Web Scraping/main.py
+++ Day 45 - Beautiful Soup Web Scraping/main.py
@@ -14,15 +14,31 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 # Write your code below this line 👇
 
 # Get data from URL
-response = requests.get(URL)
-webpage = response.text
-soup = BeautifulSoup(webpage, "html.parser")
+def get_data_from_url():
+    response = requests.get(URL)
+    webpage = response.text
+    soup = BeautifulSoup(webpage, "html.parser")
+    return soup
+
+soup = get_data_from_url()
 
 # Parse titles
-titles_tag = soup.find_all(name="h3", class_="title")
-movies = []
-for i in titles_tag:
-    movie = i.getText()
-    movies.append(movie)
-#print(movies)
-#movies.split()
+def parse_titles(input_soup):
+    titles_tag = soup.find_all(name="h3", class_="title")
+    movies = []
+    for i in titles_tag:
+        movie = i.getText()
+        movies.append(movie)
+    movies = movies[::-1]
+    return movies
+
+movies = parse_titles(soup)
+
+# Write outgoing file
+def write_file(input_movies):
+    outputfile = "movies.txt"
+    with open(outputfile, mode="w", encoding="utf8") as f:
+        for i in input_movies:
+            f.writelines(str(i)+"\n")
+
+write_file(movies)
