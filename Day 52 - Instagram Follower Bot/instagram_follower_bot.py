31013af7503fdcfca9e46b6562b21fe9121216a7
vanquirius-sala <marcelogoes@gmail.com>
2022-04-09 22:56:49 -0300

    new version

--- Day 52 - Instagram Follower Bot/instagram_follower_bot.py
+++ Day 52 - Instagram Follower Bot/instagram_follower_bot.py
@@ -14,6 +14,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
+WAIT_TIME = 5
+
 class InstagramBot:
     def __init__(self, input_user, input_password, input_account_to_follow):
         # Start Chrome Driver
@@ -30,7 +32,7 @@ class InstagramBot:
         # Login into Instagram
         self.driver.get(self.url)
         print("Going to " + self.url)
-        time.sleep(2)
+        time.sleep(WAIT_TIME)
 
         # Instagram Classes
         do_not_save_class = "sqdOP.yWX7d.y3zKF"
@@ -40,48 +42,48 @@ class InstagramBot:
         username_box = self.driver.find_element(By.NAME, "username")
         username_box.send_keys(self.user)
         print("Typing username")
-        time.sleep(1)
+        time.sleep(WAIT_TIME)
         password_box = self.driver.find_element(By.NAME, "password")
         password_box.send_keys(self.password)
         print("Typing password")
-        time.sleep(1)
+        time.sleep(WAIT_TIME)
         password_box.send_keys(Keys.RETURN)
-        time.sleep(2)
+        time.sleep(WAIT_TIME)
 
         # Do not save login information
         do_not_save_button = self.driver.find_element(By.CLASS_NAME, do_not_save_class)
         do_not_save_button.click()
         print("Do not save login information")
-        time.sleep(3)
+        time.sleep(WAIT_TIME)
 
         # Do not send notifications
         do_not_notify_button = self.driver.find_element(By.CLASS_NAME, do_not_notify_class)
         do_not_notify_button.click()
         print("Do not send notifications")
-        time.sleep(3)
+        time.sleep(WAIT_TIME)
 
     def find_followers(self):
         # Instagram Classes
         followers_xpath = "/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/div"
-        scroll_down_xpath = "/html/body/div[4]/div/div/div[2]"
+        scroll_down_xpath = "/html/body/div[6]/div/div/div/div[2]"  # this is the div for the pop-up frame and can be inspected
 
         # Go to account to follow
         self.driver.get(self.url_account_to_follow)
         print("Going to " + self.url_account_to_follow)
-        time.sleep(2)
+        time.sleep(WAIT_TIME)
 
         # Click on followers
         followers_button = self.driver.find_element(By.XPATH, followers_xpath)
         followers_button.click()
         print("Looking at the followers")
-        time.sleep(2)
+        time.sleep(WAIT_TIME)
 
         # Scroll down as much as possible
         modal = self.driver.find_element(By.XPATH, scroll_down_xpath)
         for i in range(10):
             try:
                 self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
-                time.sleep(2)
+                time.sleep(WAIT_TIME)
             except NoSuchElementException:
                 pass
 
@@ -96,7 +98,7 @@ class InstagramBot:
         for i in follow_buttons:
             try:
                 i.click()
-                time.sleep(1)
+                time.sleep(WAIT_TIME)
             except ElementClickInterceptedException:
                 cancel_button = self.driver.find_element(By.XPATH, cancel_xpath)
                 cancel_button.click()
