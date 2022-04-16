c46b56d2cbc3a03733dada1753d030ef7c0e97ed
Marcelo Góes <marcelogoes@gmail.com>
2022-04-16 11:12:11 -0300

    'new version'

--- Day 32 - Automated Birthday Wisher/main.py
+++ Day 32 - Automated Birthday Wisher/main.py
@@ -1,26 +1,33 @@
 # coding=utf-8
 # Marcelo Ambrosio de Goes
 # marcelogoes@gmail.com
-# 2022-03-18
+# 2022-04-16
 
 # 100 Days of Code: The Complete Python Pro Bootcamp for 2022
 # Day 32 - Automated Birthday Wisher
 
-##################### Extra Hard Starting Project ######################
-
 from datetime import *
 import pandas
 import random
-import send_email
+from send_email import SendEmail
 import os
 
 # Constants
 DATAFILE = "birthdays.csv"
 
+# Set as 1 to choose SendGrid over SMTP
+sendgrid_enabled = 1
+
+# E-Mail server settings
+smtp_server = "smtp.gmail.com"  # if using SMTP
+smtp_port = "587"  # if using SMTP
+
 # Credentials
-my_email = os.getenv("my_email")
-my_password = os.getenv("my_password")  # If using SMPT
-sendGridToken = os.getenv("sendGridToken")  # if using Send Grid
+from_email = os.getenv("from_email")
+sendGridToken = os.getenv("sendGridToken")  # If using SendGrid
+email_password = os.getenv("email_password")  # If using SMTP
+
+sendemail = SendEmail(from_email, smtp_server, smtp_port, email_password, sendGridToken)
 
 
 def select_birthday():
@@ -41,18 +48,17 @@ def select_birthday():
         letter = "letter_templates/letter_" + str(random.randint(1, 3)) + ".txt"
         if month_day == month_day_birth:
             with open(letter, "r", encoding="utf-8") as f:
+                subject = "Feliz aniversário! Happy Birthday!"
                 content = f.read()
                 content = content.replace("[NAME]", birthday_person["name"])
-                subject = "Feliz aniversário! Happy Birthday!"
+                # send_msg is the concatenation of subject and content
                 send_msg = ("Subject:" + str(subject) + "\n\n" + str(content)).encode('utf-8')
-                address = birthday_person["email"]
+                to_email = birthday_person["email"]
                 # 4. Send the letter generated in step 3 to that person's email address.
-                if send_email.sendgrid_enabled == 1:
-                    send_email.send_grid_email(input_mail_subject=subject, input_send_msg=content, input_to_email=address,
-                                               input_my_email=my_email, input_sendGridToken=sendGridToken)
+                if sendgrid_enabled == 1:
+                    sendemail.send_grid_email(to_email, subject, content)
                 else:
-                    send_email.send_email(input_to_email=address, input_send_msg=send_msg, input_my_email=my_email,
-                                          input_my_password=my_password)
+                    sendemail.send_smtp_email(to_email, send_msg)
 
 
 select_birthday()
--- Day 32 - Automated Birthday Wisher/send_email.py
+++ Day 32 - Automated Birthday Wisher/send_email.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # Marcelo Ambrosio de Goes
 # marcelogoes@gmail.com
-# 2022-03-18
+# 2022-04-16
 
 # 100 Days of Code: The Complete Python Pro Bootcamp for 2022
 # Day 32 - Automated Birthday Wisher
@@ -10,35 +10,34 @@ import smtplib
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-# Set as 1 to choose SendGrid over SMTP
-sendgrid_enabled = 1
+class SendEmail:
+    def __init__(self, from_email, smtp_server, smtp_port, password, sendGridToken):
+        self.from_email = from_email
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.password = password
+        self.sendGridToken = sendGridToken
 
-# E-Mail server settings
-smtp_server = "smtp.gmail.com"  # if using SMTP
-smtp_port = "587"  # if using SMTP
+    # Send E-Mail via SendGrid
+    def send_grid_email(self, to_email, mail_subject, send_msg):
+        message = Mail(
+            from_email=self.from_email,
+            to_emails=to_email,
+            subject=str(mail_subject),
+            html_content=str(send_msg).encode("utf_8").decode("unicode_escape")
+        )
+        try:
+            sg = SendGridAPIClient(self.sendGridToken)
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
 
-
-# Connect and send
-def send_email(input_smtp_server=smtp_server, input_smtp_port=smtp_port, input_my_email=".",
-               input_my_password=".", input_to_email="", input_send_msg=""):
-    with smtplib.SMTP(smtp_server, smtp_port) as connection:
-        connection.starttls()
-        connection.login(user=input_my_email, password=input_my_password)
-        connection.sendmail(from_addr=input_my_email, to_addrs=input_to_email, msg=input_send_msg)
-
-
-def send_grid_email(input_mail_subject, input_send_msg, input_to_email, input_my_email, input_sendGridToken):
-    message = Mail(
-        from_email=input_my_email,
-        to_emails=input_to_email,
-        subject=str(input_mail_subject),
-        html_content=str(input_send_msg).encode("utf_8").decode("unicode_escape")
-    )
-    try:
-        sg = SendGridAPIClient(input_sendGridToken)
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(e.message)
+    # Send E-Mail via SMTP
+    def send_smtp_email(self, to_email, send_msg):
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as connection:
+            connection.starttls()
+            connection.login(self.from_email, self.password)
+            connection.sendmail(self.from_email, to_email, send_msg)
