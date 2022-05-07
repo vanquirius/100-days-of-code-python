af390330002a6fee87f6a85ed9ac51437755291a
Marcelo Góes <marcelogoes@gmail.com>
2022-05-07 13:01:40 -0300

    new version

---
+++ Day 81 - Text to Morse Code Converter/main.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# Marcelo Ambrosio de Goes
+# marcelogoes@gmail.com
+# 2022-05-07
+
+# 100 Days of Code: The Complete Python Pro Bootcamp for 2022
+# Day 81 - Text to Morse Code Converter
+
+# You will use what you've learnt to create a text-based (command line) program
+# that takes any String input and converts it into Morse Code.
+
+# Wikipedia for Morse Code
+# https://en.wikipedia.org/wiki/Morse_code
+
